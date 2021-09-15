899586c6f8325818d52ae7214187d611770c1981
LeonardoOliveira3010 <65436800+LeonardoOliveira3010@users.noreply.github.com>
2021-09-15 14:52:42 -0300

    Ajustando o App para JSX

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from '@material-ui/core';
+
+function App() {
+  return (
+    <>
+      <Button variant="contained" color="primary">Clique bitch</Button>
+    </>
+  );
+}
+
+export default App;
